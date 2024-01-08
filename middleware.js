f14b888a02143fce20f87e62a5ab986b9043d453
Ajay <76550185+ajaynanba@users.noreply.github.com>
2024-01-08 12:53:04 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,11 +4,9 @@ export default function middleware(req) {
   return next({
     headers: {
       'Referrer-Policy': 'origin-when-cross-origin',
-      'X-Frame-Options': 'DENY',
+      'X-Frame-Options': 'ALLOW',
       'X-Content-Type-Options': 'nosniff',
       'X-DNS-Prefetch-Control': 'on',
-      'Strict-Transport-Security':
-        'max-age=31536000; includeSubDomains; preload',
     },
   });
 }
